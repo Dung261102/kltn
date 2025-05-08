53cecc691a2f5054ebcfa0578a10fd4c9b6cdf6f
Dung261102 <20521211@gm.uit.edu.vn>
2025-05-08 22:36:54 +0700

    get all the data from table

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -10,8 +10,17 @@ class TaskController extends GetxController {
     super.onReady();
   }
 
+  var taskList = <Task>[].obs;
+
 
   Future<int> addTask({Task? task}) async {
     return await DBHelper.insert(task);
+  }
+
+  //get all the data from table
+
+  void getTask() async {
+    List<Map<String, dynamic>> tasks = await DBHelper.query();
+    taskList.assignAll(tasks.map((data) => new Task.fromJson(data)).toList());
   }
 }
